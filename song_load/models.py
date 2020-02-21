ca555ba3c7012066c3b52f384d5a0ff19b929808
Вячеслав Гусев <gusevworkhard@gmail.com>
2020-02-21 13:37:12 +0000

    Upload method on server now saves song in db, hash song, return url to processed song

--- song_load/models.py
+++ song_load/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class ProcessedSong(models.Model):
+    hash_code = models.CharField(max_length=32, primary_key=True)
+    procesed_song_url = models.URLField(max_length=200)
+
+    def __str__(self):
+        return self.hash_code
