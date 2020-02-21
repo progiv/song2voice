17d35146bc3a78e4826016d9dccb6f66cc5cff34
Вячеслав Гусев <gusevworkhard@gmail.com>
2020-02-21 19:37:59 +0300

    Add model to server

--- song_load/models.py
+++ song_load/models.py
@@ -3,7 +3,9 @@ from django.db import models
 # Create your models here.
 class ProcessedSong(models.Model):
     hash_code = models.CharField(max_length=32, primary_key=True)
-    procesed_song_url = models.URLField(max_length=200)
+    input_url = models.URLField()
+    vocal_url = models.URLField()
+    accompaniment_url = models.URLField()
 
     def __str__(self):
         return self.hash_code
