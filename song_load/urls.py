8448170427fa2da41e1324401187c535f078a84c
Вячеслав Гусев <gusevworkhard@gmail.com>
2020-03-11 22:24:46 +0000

    Revert "Switch DB from sqlite to postgres and load secrets from environment"
    I messed up while rebasing the branch. This commit fixes it.
    This reverts commit 2a237c6edc1e111bd05d2e7c110212f19464e6c8.

--- song_load/urls.py
+++ song_load/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('upload', views.upload, name='song uploading')
+    path('upload', views.upload, name='song_uploading'),
+    path('download', views.download, name='get_song_vocal')
 ]
